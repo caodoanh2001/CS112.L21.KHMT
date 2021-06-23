cdbe5e4ee6cba55b7a45b851bbc79c5643b51c56
caodoanh2001 <19521366@gm.uit.edu.vn>
2021-06-24 00:40:44 +0700

    update

---
+++ Seminar/Homework_cac_nhom/RollDice/Nhom14/solution.py
@@ -0,0 +1,13 @@
+def findWays(m,n,x):
+    table=[[0]*(x+1) for i in range(n+1)]
+     
+    for j in range(1,min(m+1,x+1)):
+        table[1][j]=1
+    for i in range(2,n+1):
+        for j in range(1,x+1):
+            for k in range(1,min(m+1,j)):
+                table[i][j]+=table[i-1][j-k]
+    return table[-1][-1]
+
+k, n = [int(x) for x in input().split()]
+print(findWays(6,k,n))
